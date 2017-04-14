196a0111812e1cb77533c6b32f90f2eed057a6f9
Diegoni <romain_diegoni@hotmail.com>
2017-04-14 11:47:40 +0100

    Add brand in the toolbar component itself

--- lib/components/Toolbar.jsx
+++ lib/components/Toolbar.jsx
@@ -2,14 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Toolbar extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { show: false };
+    }
+
+    handle(event) {
+      event.preventDefault();
+
+      this.setState({ show: !this.state.show });
+    }
+
+    close(event) {
+      this.setState({ show: false });
+    }
+
     render() {
       return (
-        <header className="Toolbar">
-          <a href="#" className="Toolbar__Icon" id="toolbar__icon"></a>
-          <div className='Toolbar__Menu'>
-            {this.props.children}
-          </div>
-        </header>
+        <div className="Toolbar" onMouseLeave={(event) => this.close(event)}>
+          <a href="#" className="Toolbar__Icon" onClick={(event) => this.handle(event)}></a>
+          <a href="#" className="Toolbar__Brand">Brand</a>
+          {
+            this.state.show && (
+              <div className='Toolbar__Menu'>
+                {this.props.children}
+              </div>
+            )
+          }
+        </div>
       );
     }
 }
